2be00c5cfaddf4c1c1e83d73f7d9edc18d348832
anirudh senani <55765373+Anirudh-Senani@users.noreply.github.com>
2026-09-16 15:02:27 +0530

    Step 10: aggregate_messages

--- model.py
+++ model.py
@@ -143,8 +143,28 @@ def compute_messages(node_features, src, dst, message_fn, edge_attr=None):
 
     return message
 
-# Step 10 - aggregate_messages (not yet solved)
-# TODO: implement
+# Step 10 - aggregate_messages
+def aggregate_messages(messages, dst, num_nodes, aggr='sum'):
+    """Aggregate edge messages onto destination nodes using sum, mean, or max.
+
+    Args:
+        messages: FloatTensor of shape (E, M) with one message vector per edge.
+        dst: LongTensor of shape (E,) with destination node index for each edge.
+        num_nodes: int, number of nodes N in the graph.
+        aggr: str in {'sum', 'mean', 'max'} selecting the reduction.
+
+    Returns:
+        FloatTensor of shape (N, M); row j is the aggregated message for node j.
+    """
+    # TODO: Aggregate edge messages onto destination nodes via sum/mean/max...
+    if aggr == 'sum':
+        out = scatter_sum_to_nodes(messages, dst, num_nodes)
+    elif aggr == 'mean':
+        out = scatter_mean_to_nodes(messages, dst, num_nodes)
+    else:
+        out = scatter_max_to_nodes(messages, dst, num_nodes)
+
+    return out
 
 # Step 11 - update_node_features (not yet solved)
 # TODO: implement
